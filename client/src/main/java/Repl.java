1cdf7b73a853977a2ff382dcbf928a5d27cba87c
ethanmorehead0 <ethanmorehead0@gmail.com>
2024-11-14 14:32:29 -0700

    Repl placed with PetShop values

--- client/src/main/java/Repl.java
+++ client/src/main/java/Repl.java
@@ -1,11 +1,33 @@
 import ui.*;
 
+import java.util.Scanner;
+
 public class Repl{
-    private final PreloginClient client;
+    private final PreloginClient preloginClient;
 
-    public Repl(String serverUrl) {client = new PreloginClient();}
+    public Repl(String serverUrl) {
+        preloginClient = new PreloginClient();}
 
-    public void run() {}
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the Chess. Sign in to start.");
+        System.out.print(preloginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = preloginClient.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
 
 
     private void printPrompt() {
