f1ee680a494823838ee568fa1923c67908cae1b7
ethanmorehead0 <ethanmorehead0@gmail.com>
2024-11-14 15:40:09 -0700

    PostloginClient outlined

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -1,4 +1,45 @@
+
 package ui;
 
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import server.ServerFacade;
+
 public class PostloginClient {
+    private final ServerFacade server;
+
+
+    public PostloginClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                //case "signin" -> signIn(params);
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+
+
+    public String help() throws ResponseException {
+        return """
+            Options:
+            - List current games: "l", "list"
+            - Create a new game: "c", "create" <GAME NAME>
+            - Join a game: "j", "join" <GAME ID> <COLOR>
+            - Watch a game: "Logout"
+            - Help: "h", "help"
+            """;
+    }
+
 }
