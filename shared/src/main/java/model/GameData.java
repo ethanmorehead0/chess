25ee793b72a162f0de9b63ece55af292ed012e8e
ethanmorehead0 <ethanmorehead0@gmail.com>
2024-11-14 19:50:46 -0700

    PostloginClient list and create updated

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -3,5 +3,9 @@ package model;
 /*public record GameData(Integer integer, String s, String s1, String s2) {
 }*/
 public record GameData (Integer gameID, String whiteUsername, String blackUsername, String gameName) {
-
+    @Override
+    public String toString() {
+        return gameID + ". Game Name: " + gameName + ": White Username - '" + whiteUsername +
+                "', Black Username- '" + blackUsername + "'";
+    }
 }
