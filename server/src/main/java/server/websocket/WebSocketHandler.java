b8972b4e21013481d85ee43eabe906447750afc3
ethanmorehead0 <ethanmorehead0@gmail.com>
2024-12-07 15:43:54 -0700

    WebSocketHandler outlined

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,45 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.messages.*;
+import websocket.commands.*;
+
+import java.io.IOException;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException, ResponseException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(command.getAuthToken(), session);
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect(String AuthToken, Session session) throws IOException {
+
+    }
+
+    private void makeMove() throws ResponseException {
+
+    }
+
+    public void leave() throws IOException {
+
+    }
+
+    public void resign() throws ResponseException {
+
+    }
+}
